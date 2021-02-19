092e32f896e1617151b3689e9e32a310218e33d8
IvanCalvo <34004255+IvanCalvo@users.noreply.github.com>
2021-02-19 20:13:19 -0300

    Add shadows
    - Formato para a criação das sombras adicionado nos pilares e paredes
    - Script da câmera alterado para altura (eixo z) fixa em -80 (para que as sombras sejam visíveis)

--- Terror-2D/Assets/Scripts/FollowSpider.cs
+++ Terror-2D/Assets/Scripts/FollowSpider.cs
@@ -9,6 +9,6 @@ public class FollowSpider : MonoBehaviour
       
     void Update () 
     {
-        transform.position = new Vector3 (player.position.x + offset.x, player.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
+        transform.position = new Vector3 (player.position.x + offset.x, player.position.y + offset.y, -80f); // Camera follows the player with specified offset position
     }
 }
