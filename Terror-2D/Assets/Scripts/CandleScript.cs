b2665bd497db437cc2fea169c405fe59cf330f61
IvanCalvo <34004255+IvanCalvo@users.noreply.github.com>
2021-02-23 20:25:29 -0300

    Candle
    - Criação de script para o funcionamento da vela estática no mapa

---
+++ Terror-2D/Assets/Scripts/CandleScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class CandleScript : MonoBehaviour
+{
+    private BoxCollider2D bc;
+    private Rigidbody2D rb;
+    public Light2D luz;
+
+    //mode = 0 - apagada -- mode = 1 - acesa
+    public int mode = 0;
+
+    void Awake()
+    {
+        bc = gameObject.GetComponent<BoxCollider2D>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        luz.intensity = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(mode == 0)
+        {
+            luz.intensity = 0;
+        }
+
+        if(mode == 1)
+        {
+            luz.intensity = 1;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown("e"))
+            {
+                if (mode == 0)
+                {
+                    mode = 1;
+                }
+                else
+                {
+                    mode = 0;
+                }
+            }
+        }
+    }
+}
