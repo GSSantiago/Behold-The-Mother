2bf2e7575ca238f1f8afb4dba70b60a0a81339b5
IvanCalvo <34004255+IvanCalvo@users.noreply.github.com>
2021-03-09 00:28:43 -0300

    Update SanityScript
    -Script da imagem da sanidade atualizado para incluir uma animação que simula uma pulsação

--- Terror-2D/Assets/Scripts/SanityScript.cs
+++ Terror-2D/Assets/Scripts/SanityScript.cs
@@ -5,18 +5,86 @@ using UnityEngine.UI;
 
 public class SanityScript : MonoBehaviour
 {
-    public float sanity_value;
+    float sanity_value;
     public Image sanity_image;
     Color cor_atual;
+    bool ciclo = false;
+    int i;
+    float timeCounter;
 
     void Start()
     {
+        sanity_value = 0;
         cor_atual = Color.white;
+        cor_atual.a = sanity_value;
+        sanity_image.color = cor_atual;
     }
 
     void Update()
     {
-        cor_atual.a = sanity_value;
-        sanity_image.color = cor_atual;
+        if(Input.GetKey("h"))
+        {
+            sanity_value += 0.01f;
+            Debug.Log(sanity_value);
+
+        }
+
+        if(sanity_value >= 1)
+        {
+            sanity_value = 0;
+        }
+
+        if(sanity_value < 0.2)
+        {
+            timeCounter = 0.1f;
+        } 
+        
+        else if (sanity_value < 0.4)
+        {
+            timeCounter = 0.05f;
+        } 
+        
+        else if(sanity_value < 0.6)
+        {
+            timeCounter = 0.01f;
+        } 
+        
+        else if(sanity_value < 0.8)
+        {
+            timeCounter = 0.005f;
+        } 
+        
+        else
+        {
+            timeCounter = 0.001f;
+        }
+
+        if(ciclo == false)
+        {
+            StartCoroutine(pulse());
+        }
+    }
+
+    IEnumerator pulse()
+    {
+        ciclo = true;
+
+        for (i = 0; i < (sanity_value*100); i++)
+        {
+            cor_atual.a = (float) i / 100;
+            sanity_image.color = cor_atual;
+            yield return new WaitForSeconds(timeCounter);
+            
+        }
+
+        for (i=(int)(sanity_value * 100); i > 0; i--)
+        {
+            cor_atual.a = (float)i / 100;
+            sanity_image.color = cor_atual;
+            yield return new WaitForSeconds(timeCounter);
+
+        }
+
+        ciclo = false;
     }
 }
