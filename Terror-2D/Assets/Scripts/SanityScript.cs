cc88784c950cb9593b729ac425a44e7ed5883b75
IvanCalvo <34004255+IvanCalvo@users.noreply.github.com>
2021-03-08 21:34:01 -0300

    Adicionado efeito de teste da sanidade
    - adicionado efeito bem simples para testar a funcionalidade da sanidade

---
+++ Terror-2D/Assets/Scripts/SanityScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SanityScript : MonoBehaviour
+{
+    public float sanity_value;
+    public Image sanity_image;
+    Color cor_atual;
+
+    void Start()
+    {
+        cor_atual = Color.white;
+    }
+
+    void Update()
+    {
+        cor_atual.a = sanity_value;
+        sanity_image.color = cor_atual;
+    }
+}
