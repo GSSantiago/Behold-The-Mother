8020bcb56cb61485b5b64462edf68bf68bd3fa3f
IvanCalvo <34004255+IvanCalvo@users.noreply.github.com>
2021-03-12 12:14:42 -0300

    Upper West Shadows
    - Sombras adicionadas na "Upper West Wing"
    -Menu de pausa simples adicionado
    -Pacote "TextMeshPro" adicionado ao projeto

---
+++ Terror-2D/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    public static bool GamePaused = false;
+    public GameObject PauseMenu;
+
+    private void Start()
+    {
+        PauseMenu.SetActive(false);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    void Pause()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+}
