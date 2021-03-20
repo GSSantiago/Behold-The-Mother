831f89af510a9aec4148b6a49bdfc7eae3207c6f
IvanCalvo <34004255+IvanCalvo@users.noreply.github.com>
2021-03-20 11:23:59 -0300

    Update Shadows
    - Atualização da funcionalidade onde a lanterna para de fazer sombras quando sobrepõe alguma parte da parede

--- Terror-2D/Assets/Scripts/BehindScript.cs
+++ Terror-2D/Assets/Scripts/BehindScript.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering.Universal;
 
-public class BehindScript : MonoBehaviour
+    public class BehindScript : MonoBehaviour
 {
     public Light2D luz;
-    public BoxCollider2D coll;
+    public BoxCollider2D boxColl;
 
     void Start()
     {
         luz.shadowIntensity = 1;
     }
 
-    void OnTriggerStay2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "baseParede")
         {
@@ -23,7 +23,6 @@ public class BehindScript : MonoBehaviour
 
     void OnTriggerExit2D(Collider2D collision)
     {
-
         if (collision.gameObject.tag == "baseParede")
         {
             luz.shadowIntensity = 1;
