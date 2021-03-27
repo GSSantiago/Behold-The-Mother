b8bbf81afccf23c684845382029c92451d6db5c4
IvanCalvo <34004255+IvanCalvo@users.noreply.github.com>
2021-03-26 21:11:41 -0300

    - Update menu inicial
    - Novos botões adicionados, e alinhamento de texto no protótipo menu inicial

---
+++ Terror-2D/Assets/Scripts/RotateSpider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RotateSpider : MonoBehaviour
+{
+
+    public Image spider;
+    float rotationZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        diff.Normalize();
+        rotationZ = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+    }
+
+}
